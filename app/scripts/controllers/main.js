4cf92febcb44056cda6530f99d55a7892e0e5e63
=dugouchet <=dugouchet.a@gmail.com>
2017-02-17 19:41:22 +0100

    added angulasJS for frontEnd

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name accoteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the accoteApp
+ */
+angular.module('accoteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
